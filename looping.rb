38d7eae4afc64f03c9ee2ee83d2d5dca0435995f
Seth David Gyimah <esec26@gmail.com>
2022-08-19 03:56:48 +0000

    control flow loops

--- looping.rb
+++ looping.rb
@@ -1,5 +1,10 @@
 def happy_new_year
-  # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -16,9 +21,32 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  (1..100).each {
+    |num|
+    if num % 3 == 0 && num % 5 == 0
+      num = "FizzBuzz"
+    elsif num % 3 == 0
+      num = "Fizz"
+    elsif num % 5 == 0 
+      num = "Buzz"
+    end
+    puts num
+  }
 end
 
+# create loopcounter and set to str.length
+# create var to store reversed words
+# start loop countdown, while loop is greater than 0, decrease loop by 1, use loop's current 
+# value as index to find it's corresponding value from str and save to new_word,
+# repeat until all str values has been indexed and loop's value is = 0, and the while loop condition is no longer true
+# return new_word
+
 def reverse_string(str)
-  # your code here
+  loop = str.length
+  new_word = ""
+  while loop > 0
+    loop -= 1
+    new_word += str[loop]
+  end
+  return new_word
 end
